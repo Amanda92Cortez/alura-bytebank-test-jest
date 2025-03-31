0a4c81a7a5eee14da48f44dd000f9ca666d237df
Amanda Cortez <amanda_cortez_2012@hotmail.com>
2025-03-31 11:10:01 -0300

    Delete src directory

--- src/__tests/Cabecalho.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Cabecalho from '../componentes/Cabecalho/index';
-// import Cabecalho from '../componentes/Cabecalho/index';
-
-test('Deve renderizar o nome do usuário logado', () => {
-  render(<Cabecalho />);
-  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
-  expect(nomeUsuario).toBeInTheDocument();
-});
